96e73bce097108a79e04748d3f23a989d45d8171
Daniel Aibar <mrdbar@gmail.com>
2021-12-30 22:58:09 -0800

    Initial commit

---
+++ DataAccess/AppDbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using beanstalk_net.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace beanstalk_net.DataAccess
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContext options) : base(options)
+        {
+        }
+        
+        public virtual DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var now = DateTime.UtcNow;
+            
+            var notes = new List<Note>()
+            {
+                new Note
+                {
+                    Id = 1,
+                    Content = "Finish YouTube video",
+                    UpdatedOn = now,
+                    CreatedOn = now
+                },
+                new Note
+                {
+                    Id = 2,
+                    Content = "Go to the gym",
+                    UpdatedOn = now,
+                    CreatedOn = now
+                },
+                new Note
+                {
+                    Id = 3,
+                    Content = "Go to the grocery market",
+                    UpdatedOn = now,
+                    CreatedOn = now
+                }
+                
+                
+                
+                
+            }
+        }
+    }
+}
