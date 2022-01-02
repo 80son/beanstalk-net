95213801310d7421d003a007295f74f714dcd4be
Daniel Aibar <mrdbar@gmail.com>
2022-01-01 23:35:51 -0800

    Added notes controller and view models

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using beanstalk_net.DataAccess;
+using beanstalk_net.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace beanstalk_net.Controllers
+{
+    public class NotesController : Controller
+    {
+        private readonly ILogger<NotesController> _logger;
+        private AppDbContext _dbContext;
+
+        public NotesController(
+            ILogger<NotesController> logger,
+            AppDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext; 
+        }
+            
+           
+
+        public async Task<IActionResult> Index()
+        {
+            var notes = await _dbContext.Notes
+                .Select(note => new NoteViewModel
+            {
+                Id = note.Id,
+                CreatedOn = note.CreatedOn,
+                UpdatedOn = note.UpdatedOn,
+                Content = note.Content
+            }).ToListAsync();
+
+            var notesViewModel = new NotesViewModel {Notes = notes};
+            
+            return View(notesViewModel);
+        }
+    }
+}
---
+++ Migrations/20220102071324_initial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace beanstalk_net.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    UpdatedOn = table.Column<DateTime>(nullable: false),
+                    Content = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Notes",
+                columns: new[] { "Id", "Content", "CreatedOn", "UpdatedOn" },
+                values: new object[,]
+                {
+                    { 1, "Finish YouTube video", new DateTime(2022, 1, 2, 7, 13, 24, 611, DateTimeKind.Utc).AddTicks(3802), new DateTime(2022, 1, 2, 7, 13, 24, 611, DateTimeKind.Utc).AddTicks(3802) },
+                    { 2, "Go to the gym", new DateTime(2022, 1, 2, 7, 13, 24, 611, DateTimeKind.Utc).AddTicks(3802), new DateTime(2022, 1, 2, 7, 13, 24, 611, DateTimeKind.Utc).AddTicks(3802) },
+                    { 3, "Go to the grocery market", new DateTime(2022, 1, 2, 7, 13, 24, 611, DateTimeKind.Utc).AddTicks(3802), new DateTime(2022, 1, 2, 7, 13, 24, 611, DateTimeKind.Utc).AddTicks(3802) }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
